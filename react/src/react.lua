33f39a55386bee81e90fc1846bb856749ee1fc9a
piqu <l.smole@protonmail.com>
2023-10-14 11:31:01 +0200

    fixes with finding modules

--- react/src/react.lua
+++ react/src/react.lua
@@ -1,3 +1,30 @@
-local React = script:FindFirstAncestor('Packages'):FindFirstChild('react')
+local React
 
-return require(React)
+local paths = {
+  function()
+    return script.Parent.Parent.Parent['jsdotlua_react@17.0.2']['react']
+  end,
+  function()
+    return script.Parent.Parent.Parent.Parent.react
+  end,
+  function()
+    return script.Parent.Parent.Parent.Parent.React
+  end,
+  function()
+    return game.ReplicatedStorage.Packages.react
+  end,
+  function()
+    return game.ReplicatedStorage.Packages.React
+  end,
+}
+
+for _, fn in paths do
+  local success, response = pcall(fn)
+
+  if success then
+    React = require(response)
+    break
+  end
+end
+
+return React
--- react/src/rotai.lua
+++ react/src/rotai.lua
@@ -1,3 +1,30 @@
-local Rotai = script:FindFirstAncestor('Packages'):FindFirstChild('rotai')
+local Rotai
 
-return require(Rotai)
+local paths = {
+  function()
+    return script.Parent.Parent.Parent['piquu_rotai@0.1.5']['rotai']
+  end,
+  function()
+    return script.Parent.Parent.Parent.Parent.rotai
+  end,
+  function()
+    return script.Parent.Parent.Parent.Parent.Rotai
+  end,
+  function()
+    return game.ReplicatedStorage.Packages.rotai
+  end,
+  function()
+    return game.ReplicatedStorage.Packages.Rotai
+  end,
+}
+
+for _, fn in paths do
+  local success, response = pcall(fn)
+
+  if success then
+    Rotai = require(response)
+    break
+  end
+end
+
+return Rotai
